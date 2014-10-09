94d249f521b12be8d7cb1db73b9e4087b68cb0f1
francesco <cadue90@gmail.com>
2014-10-08 19:26:58 -0500

    POPUPS support

--- webapp/js/controllers/map-overlay/ChartsContainerController.js
+++ webapp/js/controllers/map-overlay/ChartsContainerController.js
@@ -13,21 +13,154 @@ function ChartsContainerController(parentController, svgContainer) {
     var self = this;
 
     var _svgContainer = svgContainer;
-    var _viewBoxWidth = 3200;
-    var _viewBoxHeight = 1000;
+
+    var _viewBoxWidth = svgContainer.attr("width");
+    var _viewBoxHeight = svgContainer.attr("height");
+    var _padding = {left: 40, top: 40, right: 0, bottom:0};
+    var _margins = {betweenPopups : 10};
+
+    var _singlePopupWidth = 500;
+    var _doublePopupWidth = _singlePopupWidth * 2  + _margins.betweenPopups;
+    var _popupHeight = 400;
+
+    var _positionsAvailable = {"top-left" : true, "top-right": true,
+                               "bottom-left" : true, "bottom-right" : true };
+    var _openedPopups = [];
 
 
     // PUBLIC METHODS
 
 
+    /**
+     * @param   size  string which specify the size of the popup "single" or "double"
+     * @returns {PopupController} PopupChartController instance
+     */
+    this.addPopup = function(size) {
+
+        var x, y;
+        var popupPosition;
+
+        //find an available position
+        if(size == "single") {
+
+            if(_positionsAvailable["top-right"]) {
+                x = _viewBoxWidth - _singlePopupWidth - _padding.left;
+                y = 0 + _padding.top;
+                popupPosition = "top-right";
+            } else if(_positionsAvailable["top-left"]) {
+                x = _viewBoxWidth - _singlePopupWidth * 2 - _padding.left - _margins.betweenPopups;
+                y = 0 + _padding.top;
+                popupPosition = "top-left";
+            } else if(_positionsAvailable["bottom-right"]) {
+                x = _viewBoxWidth - _singlePopupWidth - _padding.left;
+                y = 0 + _popupHeight + _margins.betweenPopups + _padding.top;
+                popupPosition = "bottom-right";
+            } else if(_positionsAvailable["bottom-left"]) {
+                x = _viewBoxWidth - _singlePopupWidth * 2 - _padding.left - _margins.betweenPopups;
+                y = 0 + _popupHeight + _margins.betweenPopups + _padding.top;
+                popupPosition = "bottom-left";
+            } else {
+                console.log("ERROR: no position available for the popup");
+                return null;
+            }
+
+            _positionsAvailable[popupPosition] = false
+        } else if (size == "double") {
+
+            if(_positionsAvailable["top-left"] && _positionsAvailable["top-right"]){
+                x = _viewBoxWidth - _doublePopupWidth - _padding.left;
+                y = 0 + _padding.top;
+                popupPosition = "top";
+                _positionsAvailable["top-left"] = _positionsAvailable["top-right"] = false;
+            } else if (_positionsAvailable["bottom-left"] && _positionsAvailable["bottom-right"]) {
+                x = _viewBoxWidth - _doublePopupWidth - _padding.left;
+                y = 0 + _popupHeight + _margins.betweenPopups + _padding.top;
+                popupPosition = "bottom";
+                _positionsAvailable["bottom-left"] = _positionsAvailable["bottom-right"] = false;
+            } else {
+                console.log("ERROR: no position available for the popup")
+                return null;
+            }
+
+        } else {
+            console.log(size + " is not a popup size");
+        }
+
+        var popupSvg = _svgContainer.append("svg")
+            .classed("popup-controller", true)
+            .attr("x", x)
+            .attr("y", y)
+            .attr("height", _popupHeight);
+
+        if(size == "single"){
+            popupSvg.attr("width", _singlePopupWidth);
+        } else if (size == "double"){
+            popupSvg.attr("width", _doublePopupWidth);
+        }
+
+        var popup = new PopupController(this, popupSvg, size);
+        popup.positionInsideContainer = popupPosition;
+        _openedPopups.push(popup);
+        return popup;
+    };
+
+
+    /**
+     * Close a popup given the popup instance
+     * @param popup
+     */
+    this.closePopup = function(popup) {
+
+        if(popup.getPopupSize() == "single"){
+
+            _positionsAvailable[popup.positionInsideContainer] = true;
+
+        } else if(popup.getPopupSize() == "double"){
+
+            if(popup.positionInsideContainer == "top"){
+                _positionsAvailable["top-left"] = _positionsAvailable["top-right"] = true;
+            } else if (popup.positionInsideContainer == "bottom"){
+                _positionsAvailable["bottom-left"] = _positionsAvailable["bottom-right"] = true;
+            }
+
+        }
+
+        _openedPopups = _.without(_openedPopups, popup);
+        popup.getContainerSvg().remove();
+
+    };
+
+    this.closeAllPopups = function() {
+       for(i in _positionsAvailable){
+           _positionsAvailable[i] = true;
+       }
+       for(i in _openedPopups) {
+           _openedPopups[i].getContainerSvg().remove();
+       }
+        _openedPopups = []
+    };
+
+
     // PRIVATE METHODS
     var draw = function() {
+
+        //TODO TEMP Background
+        _svgContainer.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("fill","#CCCCEE");
+
+        self.addPopup("single");
+        self.addPopup("single");
+        self.addPopup("double");
 
     };
 
     var init = function() {
+
         _svgContainer
-            .attr("viewBox", "0 0 " + _viewBoxWidth + " " + _viewBoxHeight);
+            .attr("viewBox","0 0 " + _viewBoxWidth + " " + _viewBoxHeight)
+            .attr("preserveAspectRatio","xMaxYMin meet");
         draw();
     } ();
 }
---
+++ webapp/js/controllers/map-overlay/PopupController.js
@@ -0,0 +1,94 @@
+/**
+ * @class: PopupController
+ * @description Controller of the chart popup
+ *
+ * @param parentController
+ * @param svgContainer
+ * @param size
+ */
+function PopupController(parentController, svgContainer, popupSize) {
+    // Call the base class constructor
+    ViewController.call(this, parentController);
+
+    // PRIVATE ATTRIBUTES
+    var self = this;
+
+    var _svgContainer = svgContainer;
+    var _svgChart;
+    var _viewBoxWidth;
+    var _viewBoxHeight;
+    var _popupSize = popupSize;
+    var _closeButtonSize = 30;
+
+    var _padding = {left: 14, top: 14, right: 14, bottom:14};
+
+    // PUBLIC METHODS
+    /**
+     * @returns the svg where to draw the chart
+     */
+    this.getChartSvg = function() {
+        return _svgChart;
+    };
+
+    /**
+     * @returns the container svg
+     */
+    this.getContainerSvg = function() {
+        return _svgContainer;
+    };
+
+    this.getPopupSize = function() {
+        return _popupSize;
+    };
+
+    this.closePopup = function() {
+
+    };
+
+    // PRIVATE METHODS
+    var draw = function() {
+
+        //create background rect
+        _svgContainer.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .classed("popup-controller-background", true);
+
+        //create the svg  where the chart will be drawn
+        _svgChart = _svgContainer.append("svg")
+            .attr("x", _padding.left)
+            .attr("y", _padding.top)
+            .attr("width", _viewBoxWidth - _padding.left - _padding.right)
+            .attr("height", _viewBoxHeight - _padding.top - _padding.bottom)
+            .classed("popup-controller-chart-container", true);
+
+        //Close button
+        _svgContainer.append("image")
+            .classed("popup-controller-close-button", true)
+            .attr("xlink:href", "img/popup-controller-close-button.png")
+            .attr("x", _viewBoxWidth - _padding.right - _closeButtonSize)
+            .attr("y", _padding.top)
+            .attr("width", _closeButtonSize)
+            .attr("height", _closeButtonSize)
+            .on("click", function(){parentController.closePopup(self)});
+
+    };
+
+    var init = function() {
+        _viewBoxHeight = _svgContainer.attr("height");
+        _viewBoxWidth = _svgContainer.attr("width");
+        /*if(_popupSize == "single"){
+            _viewBoxWidth = 100;
+        } else if(_popupSize == "double") {
+            _viewBoxWidth = 210;
+        }*/
+
+        _svgContainer
+            .attr("viewBox", "0 0 " + _viewBoxWidth + " " + _viewBoxHeight);
+
+        draw();
+    } ();
+}
+
+// Inheritance
+Utils.extend(PopupController, ViewController);
