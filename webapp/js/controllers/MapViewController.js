14d1324840f1c2c2d5204e5af213a0c33fa6a8da
Macs <massimodemarchi91@gmail.com>
2014-10-13 17:25:03 -0500

    Update index page of the project

--- webapp/js/controllers/MapViewController.js
+++ webapp/js/controllers/MapViewController.js
@@ -16,7 +16,7 @@ function MapViewController(parentController, htmlContainer) {
     var _htmlContainer = htmlContainer;
 
     var _mapContainer;
-    var _defaultZoom = 10;
+    var _defaultZoom = 13;
     
     var _mapID = 'krbalmryde.jk1dm68f';
     var _mapURL = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
