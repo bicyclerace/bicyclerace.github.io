706321455e72ce70c6917c5130f4df5ab854a574
Paul Murray <plmrry@gmail.com>
2014-10-21 19:02:29 +0000

    dynamic play a day without filter

--- webapp/js/controllers/ui/charts/UIPlayADayChartViewController.js
+++ webapp/js/controllers/ui/charts/UIPlayADayChartViewController.js
@@ -71,10 +71,8 @@ function UIPlayADayChartViewController(parentController) {
     };
     
     this.addData = function(d) {
-        var exists = _data.every(function(_d) { return ! _.isEqual(_d, d) });
-        if (! exists) _data.push(d);
-        // _data.push(d);
-        // _data = _.unique( _data, false, function(d) { return d.date } );
+        _data.push(d);
+        _data = _.unique( _data, false, function(d) { return d.date } );
         updateChart();
     };
 
@@ -98,48 +96,18 @@ function UIPlayADayChartViewController(parentController) {
         _xScale.range([0, _width]);
         _yScale.range([_height, 0]);
         
-        console.log("updated", _data);
-        
         var domain = _xScale.domain();
         
         var extent = d3.extent(_data, function(d) { return d.date });
         var day = d3.time.day;
         
         if (domain[0].getYear() === 69) _xScale.domain([day.floor(extent[0]), day.ceil(extent[0])]);
-        if (extent[1] > domain[1]) _xScale.domain([day.floor(extent[0]), extent[1]]);
+        if (extent[1] > domain[1]) _xScale.domain([extent[0], extent[1]]);
         
         var maxActive = d3.max(_data, function(d) { return parseFloat(d.active); });
         var yMax = (maxActive === 0) ? 10 : maxActive;
         
         _yScale.domain([0, yMax]);
-
-        // // Setup x scale
-        // var xScale = d3.scale.ordinal()
-        //     .domain(_data.map(function(d) { return d.label; }))
-        //     .rangeRoundBands([0, width], .1);
-
-        // // Setup y scale
-        // var yScale = d3.scale.linear()
-        //     .domain([0, d3.max(_data, function(d) { return parseFloat(d.value); })])
-        //     .range([height, 0]);
-
-        // // Setup x axis
-        // var xAxis = d3.svg.axis()
-        //     .scale(xScale)
-        //     .orient("bottom")
-        //     .outerTickSize(1);
-
-        // // Setup y axis
-        // var yAxis = d3.svg.axis()
-        //     .scale(yScale)
-        //     .orient("left")
-        //     .ticks(6)
-        //     //.tickFormat(formatNumber)
-        //     .outerTickSize(1);
-
-        // // Setup color scale
-        // var colorScale = d3.scale.ordinal()
-        //     .range(_columnColors);
         
         var colorScale = d3.scale.category10();
 
@@ -160,22 +128,6 @@ function UIPlayADayChartViewController(parentController) {
             .attr("transform", "translate(0," + _height + ")")
             .call(_xAxis);
 
-        // var xLabel = gxAxis.select(".x-label");
-        // if(xLabel.empty()) {
-        //     xLabel = gxAxis
-        //         .append("text")
-        //         .classed("x-label", true)
-        //         .attr("transform", "translate(" + (_width +10) + ",0)")
-        //         .attr("y", 6)
-        //         .attr("dy", ".71em")
-        //         .style("text-anchor", "start");
-        // }
-        // xLabel
-        //     .text(function() {
-        //         return _xAxisLabel;
-        //     });
-
-        // y axis
         var gyAxis = chart.select(".y.axis");
         if(gyAxis.empty()) {
             gyAxis = chart.append("g").attr("class", "y axis");
@@ -202,70 +154,23 @@ function UIPlayADayChartViewController(parentController) {
             gLines = chart.append("g").attr("class", "lines");
         }
 
+        var line = d3.svg.line()
+            .x(function(d) { return _xScale(d.date) })
+            .y(function(d) { return _yScale(d.active) });
+            
+        var filterLine = d3.svg.line()
+            .x(function(d) { return _xScale(d.date ) })
+            .y(function(d) { return _yScale(d.filtered)})
+            
+        var path = gLines.selectAll("path.active").data([_data]);
+        
+        path.enter().append("path").attr("class", "active");
+        
+        path.attr("d", function(d) { return line(d) })
+            .style({
+                fill: "none", stroke: "steelblue", "stroke-width": "3px"
+            });
 
-        // // Bars
-        // var gBars = chart.select(".bars");
-        // if(gBars.node() == null) {
-        //     gBars = chart.append("g").classed("bars", true);
-        // }
-
-        // // Update
-        // var bars = gBars
-        //     .selectAll(".bar")
-        //     .data(_data);
-
-        // bars
-        //     .attr("x", function(d) {
-        //         return xScale(d.label);
-        //     })
-        //     .attr("y", function(d) {
-        //         return yScale(d.value);
-        //     })
-        //     .attr("height", function(d) {
-        //         return height - yScale(d.value);
-        //     })
-        //     .attr("width", xScale.rangeBand())
-        //     .style("fill", function(d) {
-        //         return colorScale(d.label);
-        //     });
-
-        // // Enter
-        // bars
-        //     .enter()
-        //     .append("rect")
-        //     .attr("class", "bar")
-        //     .attr("x", function(d) {
-        //         return xScale(d.label);
-        //     })
-        //     .attr("y", function(d) {
-        //         return yScale(d.value);
-        //     })
-        //     .attr("height", function(d) {
-        //         return height - yScale(d.value);
-        //     })
-        //     .attr("width", xScale.rangeBand())
-        //     .style("fill", function(d) {
-        //         return colorScale(d.label);
-        //     });
-
-        // // Remove
-        // bars
-        //     .exit()
-        //     .remove();
-
-        // // Setup title
-        // var titleText = self.getView().getSvg().select(".title");
-        // if(titleText.node() == null) {
-        //     titleText = self.getView().getSvg().append("text").classed("title", true);
-        // }
-
-        // titleText.attr("transform", "translate(" + (_chartMargin.left + (width /2)) + "," + (_chartMargin.top/2) + ")");
-        // titleText
-        //     .attr("width", width)
-        //     .attr("height", _chartMargin.top)
-        //     .attr("text-anchor", "middle")
-        //     .attr("dy", "0.5em")
-        //     .text(_title);
     };
 
     // Init
