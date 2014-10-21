78e76864c4e1def80dac78d650f5682c97f49915
Paul Murray <plmrry@gmail.com>
2014-10-21 20:33:09 +0000

    website update

---
+++ applyBootstrap.js
@@ -0,0 +1,27 @@
+$(window).load(function() {
+    $("main").addClass("container");
+    $("nav ul").addClass("nav navbar-nav");
+    $("nav").addClass("container")
+        .wrap('<div class="navbar navbar-inverse navbar-fixed-top" role="navigation"></div>');
+    loadHash("usage");
+})
+
+$(window).on("hashchange", function() {
+    var hash = window.location.hash.substr(1);
+    loadHash(hash);
+})
+
+function loadHash(hash) {
+    selectNav(hash);
+    $("main").load(hash + ".html");
+}
+
+function selectNav(hash) {
+    var navs = $("nav li a");
+    navs.parent().removeClass("active");
+    navs
+        .filter(function() { 
+            return $(this).attr("href").substr(1) == hash 
+        })
+        .parent().addClass("active");
+}
